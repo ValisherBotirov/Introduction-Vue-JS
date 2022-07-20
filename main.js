9f3acbd6fcf25ef4ac055b325e034bdb9e7743e7
Valisher Botirov <valisherbotirov1218@gmail.com>
2022-07-20 13:56:17 +0500

    Event lessener

--- main.js
+++ main.js
@@ -2,11 +2,22 @@ console.log("hello world");
 console.log(Vue);
 
 const app = Vue.createApp({
+
   data(){
     return {
       name : "Valisher",
       age : 20,
       email : "valisherbotirov1218@gmail.com"
+    }
+  },
+
+  methods : {
+    changeAge(newAge){
+      this.age = this.age+1
+    },
+
+    changeName(){
+      this.name = "Valisher Botirov"
     }
   }
 })
